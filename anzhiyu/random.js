efa5147f068ecbe02b2a35acde452e744926e262
5182346Az <zuihongchen666@gmail.com>
2024-12-02 19:23:02 +0800

    Site updated: 2024-12-02 19:23:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/30/hello-world/","2024/11/30/这是一篇人工智能文章/","2024/11/30/这是一篇新的人工智能文章/","2024/12/01/这是一篇新的博文/","2024/12/01/这是一篇新的博文-1/"];function toRandomPost(){
+var posts=["2024/12/01/这是一篇新的博文/","2024/12/01/这是一篇新的博文-1/","2024/11/30/这是一篇新的人工智能文章/","2024/11/30/这是一篇人工智能文章/","2024/11/30/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
